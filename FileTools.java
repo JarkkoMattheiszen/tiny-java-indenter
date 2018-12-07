7da0eac7145a13133c9b656fb8cadfc89201e50e
JarkkoMattheiszen <44687760+JarkkoMattheiszen@users.noreply.github.com>
2018-12-07 16:10:58 +0200

    Code cleanup

--- FileTools.java
+++ FileTools.java
@@ -1,21 +1,3 @@
-package tiny.java.indenter;
-
-/*
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- */
-
-/**
- *
- * @author Jarkko Mattheiszen
- */
-
-
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.FileInputStream;
